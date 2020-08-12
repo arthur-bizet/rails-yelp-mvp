7d7779b6961ee1ddde511a963e1a20cec9774ccb
Arthur Bizet <arthur.m.bizet@gmail.com>
2020-08-12 16:14:18 +0200

    rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating some restaurants..."
+25.times do
+  restaurant = Restaurant.create(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber
+    )
+  puts "Restaurant with id #{restaurant.id} was created"
+end
